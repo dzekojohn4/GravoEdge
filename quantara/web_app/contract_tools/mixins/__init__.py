2a74f22142511535063548872f4a43d0e569b046
YaronZaki <kalbunahmar@gmail.com>
2026-06-11 13:12:11 +0000

    refactor: alphabetize imports and improve docstring in mixins/__init__.py
    Sorts imports alphabetically and expands the module docstring to list
    the exported mixins.

--- quantara/web_app/contract_tools/mixins/__init__.py
+++ quantara/web_app/contract_tools/mixins/__init__.py
@@ -1,10 +1,12 @@
 """
-Import all mixins here to make them available
-to the rest of the application.
+Contract-tool mixins for the GravoEdge protocol.
+
+Re-exports DashboardMixin, HealthRatioMixin, DepositMixin, AlertMixin,
+and PositionMixin for convenient access throughout the web application.
 """
 
-from .dashboard import DashboardMixin
-from .health_ratio import HealthRatioMixin
-from .deposit import DepositMixin
 from .alert import AlertMixin
+from .dashboard import DashboardMixin
+from .deposit import DepositMixin
+from .health_ratio import HealthRatioMixin
 from .position import PositionMixin
