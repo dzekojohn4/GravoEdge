14c55951953fa8880c1ccc477deadbd67772f468
YaronZaki <kalbunahmar@gmail.com>
2026-06-11 13:23:15 +0000

    docs: add JSDoc to useConnectWallet hook

--- quantara/frontend/src/hooks/useConnectWallet.js
+++ quantara/frontend/src/hooks/useConnectWallet.js
@@ -2,6 +2,12 @@ import { useMutation } from '@tanstack/react-query';
 import { notify } from '../components/layout/notifier/Notifier';
 import { connectWallet } from '../services/wallet';
 
+/**
+ * Hook for connecting to the Freighter Stellar wallet.
+ *
+ * @param {Function} setWalletId - Zustand store setter for wallet ID
+ * @returns {useMutation} React Query mutation for wallet connection
+ */
 export const useConnectWallet = (setWalletId) => {
   return useMutation({
     mutationFn: async () => {
