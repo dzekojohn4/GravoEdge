0be052c86bf386571fb54e610784f86d425cb054
Nanafancy <133337507+Nanafancy@users.noreply.github.com>
2026-06-19 15:40:18 +0100

    implemented the lending adapter (#148)

--- quantara/soroban/adapters/__init__.py
+++ quantara/soroban/adapters/__init__.py
@@ -6,6 +6,8 @@ AMM and lending protocol adapters for Stellar/Soroban.
 
 from .AMMAdapter import AMMAdapter, AMMAdapterFactory, PoolKey, PoolPrice, SwapRoute
 from .soroswap_adapter import SoroswapAMMAdapter
+from .LendingAdapter import LendingAdapter, LendingAdapterFactory, ReserveData, UserPosition
+from .blend_adapter import BlendLendingAdapter
 
 from . import _register  # noqa: F401  auto-registers concrete adapters
 
@@ -16,4 +18,9 @@ __all__ = [
     "PoolPrice",
     "SwapRoute",
     "SoroswapAMMAdapter",
+    "LendingAdapter",
+    "LendingAdapterFactory",
+    "ReserveData",
+    "UserPosition",
+    "BlendLendingAdapter",
 ]
